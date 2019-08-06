07a1f9ede8201d082dc50c812552e708c8dc4db4
Administrator <Administrator@PC-201812040840>
2019-08-06 23:37:25 +0800

    ...

--- src/mvcpattern/StaffController.java
+++ src/mvcpattern/StaffController.java
@@ -3,40 +3,21 @@
  */
 package mvcpattern;
 
-import java.util.ArrayList;
-
 /**
  * @author Administrator
  *
  */
 public class StaffController {
 
-	private Staff staff;
-	private StaffView staffView;
+	Staff staff = null;
+	StaffView staffView = null;
 	
 	public StaffController(Staff staff, StaffView staffView) {
 		this.staff = staff;
 		this.staffView = staffView;
 	}
-
-	public Staff getStaff() {
-		return staff;
-	}
-
-	public void setStaff(Staff staff) {
-		this.staff = staff;
-	}
-
-	public StaffView getStaffView() {
-		return staffView;
-	}
-
-	public void setStaffView(StaffView staffView) {
-		this.staffView = staffView;
-	}
 	
 	public void updateView() {
-		staffView.showStaffInfo(this.staff.getName(), this.staff.getId());
+		System.out.println("....");
 	}
-	
 }
--- src/mvcpattern/StaffView.java
+++ src/mvcpattern/StaffView.java
@@ -8,7 +8,7 @@ package mvcpattern;
  *
  */
 public class StaffView {
-	public void showStaffInfo(String...args) {
+	public void showStaffInfo(String[] args) {
 		for(String str : args) {
 			System.out.println(str);
 		}
